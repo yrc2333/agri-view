2221e9c6784b1b2709a8ae7bd906975ca8eeee53
Yanc <yrc741897145@qq.com>
2023-02-14 17:26:22 +0800

    🐞 fix: changelog配置

---
+++ packages/form-generate/env.d.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: Yanc
+ * @LastEditTime: 2023-02-13 17:39:16
+ */
+/// <reference types="vite/client" />
+
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+  const component: DefineComponent<{}, {}, any>;
+  export default component;
+}
